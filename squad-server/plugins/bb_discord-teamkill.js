56b7a048fe3481818b2ad4629b0c5359917019f0
Michael-Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2022-04-09 15:41:12 +0200

    adjust Timestamp format

--- squad-server/plugins/bb_discord-teamkill.js
+++ squad-server/plugins/bb_discord-teamkill.js
@@ -87,7 +87,7 @@ export default class BB_DiscordTeamkill extends DiscordBasePlugin {
 
     fields.push({
       name: 'Timestamp',
-      value: `${info.time.toString()}`,
+      value: `${info.time.toISOString()}`,
       inline: true
     });
 
