552de4f87b01eed19361da747e9ad1f8146149e3
Michael-Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2022-04-09 15:55:54 +0200

    change Steam ID

--- squad-server/plugins/bb_discord-teamkill.js
+++ squad-server/plugins/bb_discord-teamkill.js
@@ -54,7 +54,7 @@ export default class BB_DiscordTeamkill extends DiscordBasePlugin {
     const fields = [
       {
         name: "Attacker",
-        value: `[${info.attacker.name}](https://www.battlemetrics.com/rcon/players?filter%5Bsearch%5D=${info.attacker.steamID}) [${info.attacker.steamID}](https://steamcommunity.com/profiles/${info.attacker.steamID})`,
+        value: `[${info.attacker.name}](https://www.battlemetrics.com/rcon/players?filter%5Bsearch%5D=${info.attacker.steamID}) \([${info.attacker.steamID}](https://steamcommunity.com/profiles/${info.attacker.steamID})\)`,
         //inline: true
       },
       /*{
@@ -68,7 +68,7 @@ export default class BB_DiscordTeamkill extends DiscordBasePlugin {
       },
       {
         name: "Victim",
-        value: `[${info.victim.name}](https://www.battlemetrics.com/rcon/players?filter%5Bsearch%5D=${info.victim.steamID}) [${info.victim.steamID}](https://steamcommunity.com/profiles/${info.victim.steamID})`,
+        value: `[${info.victim.name}](https://www.battlemetrics.com/rcon/players?filter%5Bsearch%5D=${info.victim.steamID}) \([${info.victim.steamID}](https://steamcommunity.com/profiles/${info.victim.steamID})\)`,
         //inline: true
       },
       /*{
