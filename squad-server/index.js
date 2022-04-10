d145ea3cbf5ed083d6b9810331055c6c49b9d23f
Michael-Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2022-04-10 10:10:49 +0200

    f

--- squad-server/index.js
+++ squad-server/index.js
@@ -212,9 +212,9 @@ export default class SquadServer extends EventEmitter {
     });
 
     this.logParser.on('PLAYER_DAMAGED', async (data) => {
+
       data.victim = await this.getPlayerByName(data.victimName);
       data.attacker = await this.getPlayerByName(data.attackerName);
-
       if (data.victim && data.attacker)
         data.teamkill =
           data.victim.teamID === data.attacker.teamID &&
@@ -229,7 +229,7 @@ export default class SquadServer extends EventEmitter {
     this.logParser.on('PLAYER_WOUNDED', async (data) => {
       data.victim = await this.getPlayerByName(data.victimName);
       data.attacker = await this.getPlayerByName(data.attackerName);
-
+      Logger.verbose('LogParser', 3, `Victim Name: ${data.victimName}, Attacker Name: ${data.attackerName}, FoundVictim Name: ${data.victim}, FoundAttacker Name: ${data.attacker}`);
       if (data.victim && data.attacker)
         data.teamkill =
           data.victim.teamID === data.attacker.teamID &&
