38412c9a13ea9aa9075e20d0bdca23469977b94b
Michael-Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2022-04-11 19:12:15 +0200

    Initial commit PlayerController Tracking & attachment to Player

--- squad-server/log-parser/player-connected.js
+++ squad-server/log-parser/player-connected.js
@@ -2,13 +2,13 @@ export default {
   regex: /^\[([0-9.:-]+)]\[([ 0-9]*)]LogNet: Join succeeded: (.+)/,
   onMatch: (args, logParser) => {
     const data = {
+      ...logParser.eventStore['player-login'],
       raw: args[0],
       time: args[1],
       chainID: args[2],
-      playerSuffix: args[3],
-      steamID: logParser.eventStore['steamid-connected']
+      playerSuffix: args[3]
     };
-
+    delete logParser.eventStore['player-login'];
     logParser.emit('PLAYER_CONNECTED', data);
   }
 };
---
+++ squad-server/log-parser/player-controller-connected.js
@@ -0,0 +1,14 @@
+export default {
+  regex: /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquad: PostLogin: NewPlayer: BP_PlayerController_C .*:PersistentLevel.(.+)/,
+  onMatch: (args, logParser) => {
+    const data = {
+      ...logParser.eventStore['player-login'],
+      raw: args[0],
+      time: args[1],
+      chainID: args[2],
+      playerController: args[3]
+    };
+    logParser.eventStore['player-login'] = data;
+    logParser.emit('PLAYER_CONTROLLER_CONNECTED', data);
+  }
+};
