5cc46ea46334908430086511edd2b5bf90378828
Michael-Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2022-04-12 17:46:45 +0200

    improve TK regex for player names

--- squad-server/log-parser/player-teamkilled.js
+++ squad-server/log-parser/player-teamkilled.js
@@ -1,6 +1,6 @@
 export default {
   //[2022.04.09-21.07.47:205][355]LogSquadScorePoints: Verbose: ScorePointsDelayed: Points: -2.000000 ScoreEvent: TeamKilled Mrlegoface
-  regex: /^\[([0-9.:-]+)]\[([ 0-9]*)]LogSquadScorePoints: Verbose: ScorePointsDelayed: Points: .* ScoreEvent: TeamKilled ([A-z_0-9]+)/,
+  regex: /^\[([0-9.:-]+)\]\[([ 0-9]*)\]LogSquadScorePoints: Verbose: ScorePointsDelayed: Points: .* ScoreEvent: TeamKilled (.*)$/,
   onMatch: (args, logParser) => {
     if(logParser.eventStore["nullptr"].chainID == args[2]){
       const data = {
