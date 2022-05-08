3c65b63425846ed182e0825eac2b8177c371011e
Michael-Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2022-05-08 10:23:56 +0200

    Added BB_DiscordRoundEnd Plugin

---
+++ squad-server/plugins/bb_discord-round-end.js
@@ -0,0 +1,68 @@
+import DiscordBasePlugin from './discord-base-plugin.js';
+
+export default class BB_DiscordRoundEnd extends DiscordBasePlugin {
+  static get description() {
+    return 'The <code>BB_DiscordRoundEnd</code> plugin will send the round change details to a Discord channel.';
+  }
+
+  static get defaultEnabled() {
+    return true;
+  }
+
+  static get optionsSpecification() {
+    return {
+      ...DiscordBasePlugin.optionsSpecification,
+      channelID: {
+        required: true,
+        description: 'The ID of the channel to log admin broadcasts to.',
+        default: '',
+        example: '667741905228136459'
+      },
+      color: {
+        required: false,
+        description: 'The color of the embed.',
+        default: 16761867
+      }
+    };
+  }
+
+  constructor(server, options, connectors) {
+    super(server, options, connectors);
+
+    this.onNewGame = this.onNewGame.bind(this);
+  }
+
+  async mount() {
+    this.server.on('NEW_GAME', this.onNewGame);
+  }
+
+  async unmount() {
+    this.server.removeEventListener('NEW_GAME', this.onNewGame);
+  }
+
+  async onNewGame(info) {
+    await this.sendDiscordMessage({
+      embed: {
+        title: 'Round Ended',
+        color: this.options.color,
+        fields: [
+          {
+            name: 'Last Layer',
+            value: `${this.server.layerHistory[1].layer.name}`,
+            inline: true
+          }
+          {
+            name: 'Next Layer',
+            value: `${this.server.layerHistory[0].layer.name}`,
+            inline: true
+          }
+          {
+            name: 'Winner'
+            value: `${info.winner?info.winner:'Could not be determined.'}`
+          }
+        ],
+        timestamp: info.time.toISOString()
+      }
+    });
+  }
+}
