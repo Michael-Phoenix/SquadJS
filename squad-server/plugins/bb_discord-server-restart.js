87363e51b0d574e98e1f8b554ea4065d8daaa1c5
Michael-Phoenix <3187643-Michael-Phoenix@users.noreply.gitlab.com>
2022-05-22 07:54:07 +0200

    f

--- squad-server/plugins/bb_discord-server-restart.js
+++ squad-server/plugins/bb_discord-server-restart.js
@@ -127,10 +127,10 @@ export default class BB_DiscordServerRestart extends DiscordBasePlugin {
   async checkEmptyRestart() {
     const currentTime = new Date();
     if(!this.preBroadcastInterval && this.server.nextLayer?.rawName === this.options.restart_map) this.preBroadcastInterval = setInterval(this.preBroadcast, 3 * 60 * 1000);
-    //this.verbose(
-    //  1,
-    //  `checking for restart at : ${currentTime.toISOString()} ${currentTime.getTime()} Server Restart Time: ${this.server.lastRestartTime} Time since last restart: ${(currentTime.getTime() - this.server.lastRestartTime) / (1000 * 3600)}`
-    //);
+    this.verbose(
+      1,
+      `preBroadCast: ${this.preBroadcastInterval}, checking for restart at : ${currentTime.toISOString()} ${currentTime.getTime()} Server Restart Time: ${this.server.lastRestartTime} Time since last restart: ${(currentTime.getTime() - this.server.lastRestartTime) / (1000 * 3600)}`
+    );
     if(currentTime.getUTCHours() < this.options.restart_start ||
       currentTime.getUTCHours() >= this.options.restart_end ||
       (currentTime.getTime() - this.server.lastRestartTime) / (1000 * 3600) < this.options.time_between_restarts ||
